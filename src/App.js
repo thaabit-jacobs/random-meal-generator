4c2d3a5c1809f7997d01a80ea42aa917eda82a90
thaabit <mohammedthaabitjacobs@gmail.com>
2022-03-05 22:36:46 +0200

    working on it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props){
     super(props);
 
@@ -15,8 +15,31 @@ class App extends Component {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(response => response.json())
     .then((data) => {
-      console.log(data);
+      console.log(data["meals"][0]);
+
+      this.setState({
+        meal: data["meals"][0]
+      })
     })
+  }
+
+  getAllIngredients() {
+      let mealObj = this.state.meal;
+      return Object.keys(mealObj)
+                           .filter(mealItem => 
+                              mealItem.indexOf("strIngredient") !== -1 && mealObj[mealItem])
+                           .map(filteredKey => mealObj[filteredKey]);
+
+
+  }
+
+  insertEmbedIntoVid() {
+    let url = this.state.meal["strYoutube"];
+    let template = "https://www.youtube.com/embed/";
+
+    console.log(url.substring(0, url.substring(url.indexOf("="))));
+
+    return url;
   }
 
   render() {
@@ -27,26 +50,31 @@ class App extends Component {
         <button>GET MEAL</button>
   
         <section className='meal-instructions'>
-          <img/>
+          <img src={this.state.meal["strMealThumb"]}/>
   
           <div className='meal-instructions-details'>
-            <h2 className="mb-1">lorem lorem lorem lorem </h2>
-            <p>lorem  lorem  lorem  lorem lorem  lorem </p>
+            <h2 className="mb-1">{this.state.meal["strMeal"]} </h2>
+            <p>{this.state.meal["strInstructions"]}</p>
           </div>
         </section>
   
-        <section className='meal-meta-details'>
-          <p className='bold'>Category</p>
-          <p className='bold'>Area</p>
-          <p className='mb-1 bold'>Tags</p>
+        <section className='meal-meta-details mb-1'>
+          <p className='bold'>Category: {this.state.meal["strCategory"]}</p>
+          <p className='bold'>Area: {this.state.meal["strArea"]}</p>
+          <p className='mb-1 bold'>Tags: {this.state.meal["strTags"]}</p>
   
           <h3>Ingredients: </h3>
           <ul>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
+            {
+              this.getAllIngredients().map(ingredient => <li key={ingredient}>{ingredient}</li>)
+            }
           </ul>
+        </section>
+
+        <section className='meal-video'>
+          <iframe width="420" height="315"
+            src={this.insertEmbedIntoVid()}>
+          </iframe>
         </section>
       </main>
     );
